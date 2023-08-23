ccdc04ff6f64dbba0650a5cdc36c9ebc178cc901
wrightty <wrightty@hdsb.ca>
2023-08-23 20:55:57 +0000

    test

---
+++ test.py
@@ -0,0 +1,21 @@
+'''
+a program that draws a rectangle using turtle graphics
+'''
+
+# import the turtle module
+
+import turtle
+
+# create a turtle object called tom
+
+tom = turtle.Turtle()
+
+# draw a rectangle
+
+tom.forward(100)
+tom.left(90)
+tom.forward(50)
+tom.left(90)
+tom.forward(100)
+tom.left(90)
+tom.forward(50)
